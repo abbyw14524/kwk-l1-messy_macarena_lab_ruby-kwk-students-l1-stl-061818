4f12956aaf6e94ccb202809703f53d429258a300
abbyw14524 <awhalen111@rsdmo.org>
2018-06-19 20:40:04 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -3,6 +3,7 @@ puts "Right arm out in front of you, palm facing down"
 puts "Left arm out in front of you, palm facing down"
 puts "Turn your right palm up"
 puts "Turn your left palm up"
+puts "Place your right hand on your left shoulder"
 puts ""
 
 # Fix The Dance Instructions Below

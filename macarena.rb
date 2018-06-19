4ef71163021fd4e01b6edb3aea64ea35b352f70d
abbyw14524 <awhalen111@rsdmo.org>
2018-06-19 20:39:29 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -1,6 +1,7 @@
 puts "How To Do The Macarena:"
 puts "Right arm out in front of you, palm facing down"
 puts "Left arm out in front of you, palm facing down"
+puts "Turn your right palm up"
 puts ""
 
 # Fix The Dance Instructions Below
